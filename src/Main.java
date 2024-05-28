b4e6d0d5eba0a84312dbaa0059226219b64e849c
U K Sankalp <166250499+uk-sankalp@users.noreply.github.com>
2024-05-28 13:01:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // IntelliJ IDEA suggests fixing it..
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the codes
